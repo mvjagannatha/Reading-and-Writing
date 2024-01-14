26e82c28b7a6f969db14bdda733efcac4ccac8d3
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 18:29:53 +0530

    Add project files.

---
+++ Reading and Writing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter your name");
+        string myname= Console.ReadLine();
+        Console.WriteLine("My name is: {0}", myname);
+       // Console.WriteLine("My name is ", +myname);
+
+
+
+    }
+}
